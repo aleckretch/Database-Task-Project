3c211c38c1d76241b183cee8511f87bb6826cb2c
Alec Kretch <aleckretch@194-49.priv23.nus.edu.sg>
2016-09-12 04:41:46 -0700

    port

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
   $SQLPASSWORD = "sunzhihao";
   $SQLDATABASE = "postgres";
 
-   $database = pg_connect("host=127.0.0.1 port=5433 dbname=postgres user=postgres password=sunzhihao");
+   $database = pg_connect("host=127.0.0.1 port=5432 dbname=postgres user=postgres password=sunzhihao");
 
    if(!$database){
          echo "Error : Unable to open database<br>";
