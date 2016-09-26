a3b7afcc47d2a45764d27cfe462fe3e419b4420c
Alec Kretch <aleckretch@r-186-106-25-172.comp.nus.edu.sg>
2016-09-25 23:26:10 -0700

    lasdfasf

--- initialize.php
+++ initialize.php
@@ -22,7 +22,7 @@
      die("Error in SQL query: " . pg_last_error());
   	}
 
-	$sql = "INSERT INTO users VALUES ('Mark', '1234', 'normal'), ('Lisa', 'abcd', 'normal')";
+	$sql = "INSERT INTO users VALUES ('Mark', '1234', 'normal'), ('Lisa', 'abcd', 'normal'), ('admin', 'password', 'admin')";
 	$result = pg_query($database, $sql);
 
 	if (!$result) {
