78f5c34d60df6cc001ed18b7d9302df73602ddd1
Jeris Tanus <jeris.tanus@gmail.com>
2016-09-26 14:36:51 +0800

    Changed the task-table schema to include approved_by, and assigned (bool) attributes.

--- initialize.php
+++ initialize.php
@@ -9,13 +9,15 @@
      die("Error in SQL query: " . pg_last_error());
   	}
 
-  	$init = "CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, title VARCHAR(40) NOT NULL, 
-  		description VARCHAR(254) NOT NULL default '', 
-  		task_date date NOT NULL default CURRENT_DATE, 
-  		start_hour integer NOT NULL CHECK (start_hour >= 0 AND start_hour < 24), start_min integer NOT NULL CHECK (start_min >= 0 AND start_min < 60), 
-  		end_hour integer NOT NULL CHECK (end_hour >= 0 AND end_hour < 24), end_min integer NOT NULL CHECK (end_min >= 0 AND end_min < 60), 
-  		assigner VARCHAR(254) REFERENCES users, owner VARCHAR(254) REFERENCES users NOT NULL )";
-	
+  	$init = "CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, title VARCHAR(40) NOT NULL,
+  		description VARCHAR(254) NOT NULL default '',
+  		task_date date NOT NULL default CURRENT_DATE,
+  		start_hour integer NOT NULL CHECK (start_hour >= 0 AND start_hour < 24), start_min integer NOT NULL CHECK (start_min >= 0 AND start_min < 60),
+  		end_hour integer NOT NULL CHECK (end_hour >= 0 AND end_hour < 24), end_min integer NOT NULL CHECK (end_min >= 0 AND end_min < 60),
+  		assigner VARCHAR(254) REFERENCES users, owner VARCHAR(254) REFERENCES users NOT NULL,
+			assigned BOOLEAN default FALSE,
+			approved_by VARCHAR(254) REFERENCES users)";
+
 	$result = pg_query($database, $init);
 
 	if (!$result) {
@@ -30,7 +32,7 @@
  	}
 
     echo 'init successful';
-  
+
 
  // close connection
  pg_close($dbh);
