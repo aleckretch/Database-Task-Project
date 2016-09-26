8ec9f5f31b79d6dc88a09ae47365812ff9a7f8ff
Sun Zhihao <zsun005@e.ntu.edu.sg>
2016-09-26 14:01:51 +0800

    basically finish the create task page

--- newTask.php
+++ newTask.php
@@ -22,8 +22,38 @@
     header("location:login.php");
   }
 
-  if(isset($_POST['taskSubmission'])) {
+  $error_message = null;
+  $success = false;
 
+  if(isset($_POST['taskSubmission'])) {
+    if(isset($_POST['title']) && isset($_POST['description']) && isset($_POST['date']) && isset($_POST['starttime']) && isset($_POST['endtime'])) {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $date = $_POST['date'];
+        $starttime = $_POST['starttime'];
+        $endtime = $_POST['endtime'];
+
+        $start_arr = explode(":" , $starttime);
+        $end_arr = explode(":" , $endtime);
+
+        $start_hour = intval($start_arr[0]);
+        $start_min = intval($start_arr[1]);
+        $end_hour = intval($end_arr[0]);
+        $end_min = intval($end_arr[1]);
+
+        $sql = "INSERT INTO tasks (title , description , task_date , start_hour , start_min, end_hour, end_min, owner) VALUES('$title' , '$description' , '$date', $start_hour, $start_min , $end_hour , $end_min , '$username')";
+
+        $result = pg_query($database, $sql);
+
+        if (!$result) {
+            die("Error in SQL query: " . pg_last_error());
+        }
+
+        $success = true;
+    } else {
+      $error_message = "Info missed";
+    }
+    
   }
  ?>
 
@@ -43,10 +73,25 @@
         <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
-  
+
   <div class="row">
     <h3>Create Your new Task</h3>
   </div>
+
+  <?php 
+    if($success) {
+      echo '<div class="alert fade in  alert-success">
+                  <button type="button" class="close" data-dismiss="alert">×</button>
+                  Task created!
+            </div>';
+    } else if($error_message != null) {
+      echo '<div class="alert fade in  alert-danger">
+                  <button type="button" class="close" data-dismiss="alert">×</button>
+                  '.$error_message.'
+            </div>';
+    }
+  
+  ?>
 
   <form method="POST" action="newTask.php" >
     <div class="row">
@@ -57,7 +102,7 @@
 
       <div class="form-group">
         <label for="taskDesc">Task Description</label><br/>
-        <textarea id="taskDesc" name="description" required class="form-control">Please fill in your task description</textarea>
+        <textarea id="taskDesc" name="description" required class="form-control" placeholder="Please describe your task here"></textarea>
       </div>
 
       <div class="form-group">
