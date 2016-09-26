5ecb62c603395a9182ca568084626110a0b36bc4
Sun Zhihao <zsun005@e.ntu.edu.sg>
2016-09-26 14:14:48 +0800

    Change the task table and make create task working

--- clean-data.php
+++ clean-data.php
@@ -1,14 +1,14 @@
 <?php
 	include("config.php");
 
-	$clean = "DROP TABLE users";
+	$clean = "DROP TABLE tasks";
 	$result = pg_query($database, $clean);
 
 	if (!$result) {
 		die("Error in SQL query: " . pg_last_error());
 	}
 
-	$clean = "DROP TABLE tasks";
+	$clean = "DROP TABLE users";
 	$result = pg_query($database, $clean);
 
 	if (!$result) {
