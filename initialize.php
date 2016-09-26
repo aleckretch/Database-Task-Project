646fd53346d7a7f220064acfe6073b141b2a14a5
Sun Zhihao <zsun005@e.ntu.edu.sg>
2016-09-26 16:05:25 +0800

    reverse the database changes

--- initialize.php
+++ initialize.php
@@ -2,27 +2,29 @@
 	include("config.php");
 
   // Let's initialize the tables used, and greate a test user.
-	$init = "CREATE TABLE IF NOT EXISTS users (username VARCHAR(254) PRIMARY KEY, password VARCHAR(254), salt CHAR(32), type VARCHAR(6) CHECK(type='normal' OR type='admin'))";
+	$init = "CREATE TABLE IF NOT EXISTS users (username VARCHAR(254) PRIMARY KEY, password VARCHAR(254), type VARCHAR(6) CHECK(type='normal' OR type='admin'))";
 	$result = pg_query($database, $init);
 
 	if (!$result) {
      die("Error in SQL query in initialize.php: " . pg_last_error());
   	}
 
-  	$init = "CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, title VARCHAR(40) NOT NULL, 
-  		description VARCHAR(254) NOT NULL default '', 
-  		task_date date NOT NULL default CURRENT_DATE, 
-  		start_hour integer NOT NULL CHECK (start_hour >= 0 AND start_hour < 24), start_min integer NOT NULL CHECK (start_min >= 0 AND start_min < 60), 
-  		end_hour integer NOT NULL CHECK (end_hour >= 0 AND end_hour < 24), end_min integer NOT NULL CHECK (end_min >= 0 AND end_min < 60), 
-  		assigner VARCHAR(254) REFERENCES users, owner VARCHAR(254) REFERENCES users NOT NULL )";
-	
+  	$init = "CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, title VARCHAR(40) NOT NULL,
+  		description VARCHAR(254) NOT NULL default '',
+  		task_date date NOT NULL default CURRENT_DATE,
+  		start_hour integer NOT NULL CHECK (start_hour >= 0 AND start_hour < 24), start_min integer NOT NULL CHECK (start_min >= 0 AND start_min < 60),
+  		end_hour integer NOT NULL CHECK (end_hour >= 0 AND end_hour < 24), end_min integer NOT NULL CHECK (end_min >= 0 AND end_min < 60),
+  		assigner VARCHAR(254) REFERENCES users, owner VARCHAR(254) REFERENCES users NOT NULL,
+			assigned BOOLEAN default FALSE,
+			status VARCHAR(254) default 'pending' CHECK(status = 'pending' OR status = 'approved' OR status = 'disapproved' OR status = 'completed'))";
+
 	$result = pg_query($database, $init);
 
 	if (!$result) {
      die("Error in SQL query in initialize.php: " . pg_last_error());
   	}
 
-	$sql = "INSERT INTO users VALUES ('Mark', 'e3ce2f895ecde88c9a0ca3cfbab42644', '2f4c37ee681b2811440145f620f7b449', 'normal'), ('Lisa', '1f25376ecddf5bb9ee77573de2886e42', '4cf6fdafe5ab4f5401f682d147bfee80', 'normal'), ('admin', 'b6a7c7264267ceb82adb9e2c84d49edb', '72c2bb70b9062f123e21e34eac743f88', 'admin')";//Mark Password = 1234, Lisa = abcd, admin = password
+	$sql = "INSERT INTO users VALUES ('Mark', '1234', 'normal'), ('Lisa', 'abcd', 'normal'), ('admin', 'password', 'admin')";
 	$result = pg_query($database, $sql);
 
 	if (!$result) {
